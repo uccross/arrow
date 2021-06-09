81134ceb67285f0741fadf680a09bea79e6c3321
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-06-09 20:40:11 +0530

    fixes

--- cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
+++ cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
@@ -136,7 +136,7 @@ static arrow::Status ScanIpcObject(cls_method_context_t hctx,
                                    std::shared_ptr<arrow::Table>& t,
                                    int64_t file_size) {
   auto file = std::make_shared<RandomAccessObject>(hctx, file_size);
-  arrow::dataset::FileSource source(file, arrow::util::Compression::LZ4_FRAME);
+  arrow::dataset::FileSource source(file, arrow::Compression::LZ4_FRAME);
 
   auto format = std::make_shared<arrow::dataset::IpcFileFormat>();
   ARROW_ASSIGN_OR_RAISE(auto fragment,
